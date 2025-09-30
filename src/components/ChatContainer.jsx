9493babd9da54ea434dae562bd511a64be53668c
Micheal Peter <michealpeter040@gmail.com>
2025-09-30 18:17:36 +0100

    more pushes

---
+++ src/components/ChatContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatContainer = () => {
+  return <div className="text-white">ChatContainer</div>;
+};
+
+export default ChatContainer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import RightSidebar from "../components/RightSidebar";
+import ChatContainer from "../components/ChatContainer";
+import Sidebar from "../components/Sidebar";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [selectedUser, setSelectedUser] = useState(true);
+
+  return (
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-200 rounded-2xl overflow-hidden h-[100%] relative grid grid-cols-1 md:grid-cols-[20rem_1fr_20rem]`}
+      >
+        <Sidebar selectedUser={selectedUser} />
+
+        <ChatContainer className={selectedUser ? "" : "md:col-span-2"} />
+
+        {selectedUser && <RightSidebar />}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
